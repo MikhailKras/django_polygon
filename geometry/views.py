13edc38062aaa259e2bd1a8924b82346d3bad9e0
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-01-29 21:56:31 +0300

    first templates added

--- geometry/views.py
+++ geometry/views.py
@@ -7,17 +7,20 @@ import re
 
 def rectangle_area(request, width: int, height: int) -> HttpResponse:
     area = width * height
-    return HttpResponse(f'<h1>The area of {width}x{height} rectangle is {area}<h1/>')
+    return render(request, 'geometry/rectangle.html')
+    # return HttpResponse(f'<h1>The area of {width}x{height} rectangle is {area}<h1/>')
 
 
 def square_area(request, size: int) -> HttpResponse:
     area = size ** 2
-    return HttpResponse(f'<h1>The area of {size}x{size} square is {area}<h1/>')
+    return render(request, 'geometry/square.html')
+    # return HttpResponse(f'<h1>The area of {size}x{size} square is {area}<h1/>')
 
 
 def circle_area(request, radius: int) -> HttpResponse:
     area = round(pi * radius ** 2, 1)
-    return HttpResponse(f'<h1>The area of a circle of radius {radius} is {area}<h1/>')
+    return render(request, 'geometry/circle.html')
+    # return HttpResponse(f'<h1>The area of a circle of radius {radius} is {area}<h1/>')
 
 
 def redirect_area(request, **kwargs):
