7978623c97ea73b678b0a9515b50aa7e2a9a33bc
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-02-02 18:10:04 +0300

    navbar added

--- others/urls.py
+++ others/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views as views_others
 
 urlpatterns = [
-    path('', views_others.get_menu_page),
+    path('', views_others.get_menu_page, name='other-home'),
     path('reeves', views_others.get_info_keanu),
     path('interesting_facts', views_others.get_info_facts),
     path('people', views_others.get_info_people)
